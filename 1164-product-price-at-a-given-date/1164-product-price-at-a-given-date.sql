f310c027ce6c1c2525be9f696ac76c54a70acd55
Nidhiansh Chauhan <107105204+Nidhiansh@users.noreply.github.com>
2026-05-05 15:55:44 +0530

    Time: 1459 ms (6.76%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select distinct product_id,ifnull((select new_price from products p2 where p2.product_id=p1.product_id and p2.change_date<='2019-08-16' order by p2.change_date desc limit 1),10)  as price from products p1;
