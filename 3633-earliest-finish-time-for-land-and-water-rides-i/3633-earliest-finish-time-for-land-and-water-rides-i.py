683731d983ab6b036485f66d73add0851690331c
Nidhiansh Chauhan <107105204+Nidhiansh@users.noreply.github.com>
2026-06-02 09:53:39 +0530

    Time: 17 ms (81.08%), Space: 12.2 MB (97.3%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -7,17 +7,26 @@ class Solution(object):
         :type waterDuration: List[int]
         :rtype: int
         """
-        res=float('inf')
-        n=len(landStartTime)
-        m=len(waterStartTime)
-        for i in range(n):
-            t1=landStartTime[i]+landDuration[i]
-            for j in range(m):
-                t2=max(t1,waterStartTime[j])+waterDuration[j]
-                res=min(res,t2)
-        for i in range(m):
-            t1=waterStartTime[i]+waterDuration[i]
-            for j in range(n):
-                t2=max(t1,landStartTime[j])+landDuration[j]
-                res=min(res,t2)
-        return res
+        # res=float('inf')
+        # n=len(landStartTime)
+        # m=len(waterStartTime)
+        # for i in range(n):
+        #     t1=landStartTime[i]+landDuration[i]
+        #     for j in range(m):
+        #         t2=max(t1,waterStartTime[j])+waterDuration[j]
+        #         res=min(res,t2)
+        # for i in range(m):
+        #     t1=waterStartTime[i]+waterDuration[i]
+        #     for j in range(n):
+        #         t2=max(t1,landStartTime[j])+landDuration[j]
+        #         res=min(res,t2)
+        # return res
+        def func(a,ad,b,bd):
+            m=float('inf')
+            for i in range(len(a)):
+                m=min(m,a[i]+ad[i])
+            res=float('inf')
+            for i in range(len(b)):
+                res=min(res,max(b[i],m)+bd[i])
+            return res
+        return min(func(landStartTime,landDuration,waterStartTime,waterDuration),func(waterStartTime,waterDuration,landStartTime,landDuration))
